955d2c5407d34c286ff2d98bf7a956994e965b55
Amir Sayonara <amirsayonara@gmail.com>
2018-11-01 20:51:24 +0700

    commit init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Internet Banking</title>
+</head>
+<body>
+    <h1>INTERNET BANKING</h1>
+</body>
+</html>
