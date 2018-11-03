8ebcc2083bda74ad6858b1c39735e9345c52c038
Amir Sayonara <amirsayonara@gmail.com>
2018-11-03 14:08:53 +0700

    pembuatan halaman awal

--- index.php
+++ index.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Internet Banking</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>C07 Internet Banking</title>
 </head>
 <body>
-    <h1>INTERNET BANKING</h1>
+    <header>
+        <div class="konten">
+            <h1>C07 Net Banking</h1>
+            <nav>
+                <ul>
+                    <li><a class="dipilih">Halaman Depan</a></li>
+                    <li><a href="test">Test</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="konten-utama">
+        <div class="artikel">
+            <h2>Halaman Depan</h2>
+            <p>Selamat datang di C07 Internet Banking</p>
+        </div>
+    </div>
+    <footer>
+        <div class="konten">
+            <div class="artikel">
+                <p>C07 Internet Banking - Pengembangan Aplikasi Web (C) - 2018</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
