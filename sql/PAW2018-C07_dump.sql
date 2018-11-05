0cb4902be9f852d20e6f07e22b3a8f0cfe39b91f
Amir Sayonara <amirsayonara@gmail.com>
2018-11-05 18:19:10 +0700

    perubahan total struktur tabel

--- sql/PAW2018-C07_dump.sql
+++ sql/PAW2018-C07_dump.sql
@@ -29,25 +29,6 @@ USE banking;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jenis_pengguna`
---
-
-CREATE TABLE `jenis_pengguna` (
-  `id` char(2) NOT NULL PRIMARY KEY,
-  `keterangan` char(25) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `jenis_pengguna`
---
-
-INSERT INTO `jenis_pengguna` (`id`, `keterangan`) VALUES
-('0', 'ADMINISTRATOR'),
-('1', 'CUSTOMER');
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `jenis_transaksi`
 --
 
@@ -67,50 +48,48 @@ INSERT INTO `jenis_transaksi` (`id`, `keterangan`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `jenis_pengguna`
+--
+
+CREATE TABLE `jenis_pengguna` (
+  `id` char(2) NOT NULL PRIMARY KEY,
+  `keterangan` char(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jenis_pengguna`
+--
+
+INSERT INTO `jenis_pengguna` (`id`, `keterangan`) VALUES
+('0', 'ADMINISTRATOR'),
+('1', 'CUSTOMER');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `pengguna`
 --
 
 CREATE TABLE `pengguna` (
-  `id` varchar(25) NOT NULL PRIMARY KEY,
+  `nama_pengguna` varchar(64) NOT NULL PRIMARY KEY,
+  `sandi` varchar(64) NOT NULL,
+  `jenis_pengguna` char(2) NOT NULL,
   `email` varchar(64) NOT NULL UNIQUE,
   `nama` char(64) NOT NULL,
   `alamat` varchar(254) DEFAULT NULL,
   `nomor_hp` char(15) NOT NULL UNIQUE,
-  `aktif` enum('1', '0') NOT NULL
+  `aktif` enum('1', '0') NOT NULL,
+  FOREIGN KEY(jenis_pengguna) REFERENCES jenis_pengguna(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `pengguna`
 --
 
-INSERT INTO `pengguna` (`id`, `email`, `nama`, `alamat`, `nomor_hp`, `aktif`) VALUES
-('AB12C-ABCHD-8ABCD-88CAA', 'admin@admin.com', 'NAMA ADMIN', 'ALAMAT ADMIN', '080000000000', '1'),
-('AB12C-ABCHD-8ABCD-88CAB', 'user@user.com', 'NAMA USER', 'ALAMAT USER', '080000000001', '1'),
-('AB12C-ABCHD-8ABCD-88CAC', 'user2@user.com', 'NAMA USER DUA', 'ALAMAT USER 2', '080000000002', '1');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `akun_pengguna`
---
-
-CREATE TABLE `akun_pengguna` (
-  `nama_pengguna` varchar(64) NOT NULL PRIMARY KEY,
-  `sandi` varchar(64) NOT NULL,
-  `jenis_pengguna` char(2) NOT NULL,
-  `id_pengguna` char(25) NOT NULL,
-  FOREIGN KEY(jenis_pengguna) REFERENCES jenis_pengguna(id) ON UPDATE CASCADE,
-  FOREIGN KEY(id_pengguna) REFERENCES pengguna(id) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `akun_pengguna`
---
-
-INSERT INTO `akun_pengguna` (`nama_pengguna`, `sandi`, `jenis_pengguna`, `id_pengguna`) VALUES
-('admin', SHA2('admin', 0), '0', 'AB12C-ABCHD-8ABCD-88CAA'),
-('user', SHA2('user', 0), '1', 'AB12C-ABCHD-8ABCD-88CAB'),
-('user2', SHA2('user2', 0), '1', 'AB12C-ABCHD-8ABCD-88CAC');
+INSERT INTO `pengguna` (`nama_pengguna`, `sandi`, `jenis_pengguna`, `email`, `nama`, `alamat`, `nomor_hp`, `aktif`) VALUES
+('admin', SHA2('admin', 0), '0', 'admin@admin.com', 'NAMA ADMIN', 'ALAMAT ADMIN', '080000000000', '1'),
+('user', SHA2('user', 0), '1', 'user@user.com', 'NAMA USER', 'ALAMAT USER', '080000000001', '1'),
+('user2', SHA2('user2', 0), '1', 'user2@user.com', 'NAMA USER DUA', 'ALAMAT USER 2', '080000000002', '1');
 
 -- --------------------------------------------------------
 
@@ -120,18 +99,18 @@ INSERT INTO `akun_pengguna` (`nama_pengguna`, `sandi`, `jenis_pengguna`, `id_pen
 
 CREATE TABLE `rekening` (
   `nomor_rekening` varchar(25) NOT NULL PRIMARY KEY,
-  `id_pengguna` varchar(25) NOT NULL,
+  `nama_pengguna` varchar(64) NOT NULL,
   `aktif` enum('1', '0') NOT NULL,
-  FOREIGN KEY(id_pengguna) REFERENCES pengguna(id) ON UPDATE CASCADE
+  FOREIGN KEY(nama_pengguna) REFERENCES pengguna(nama_pengguna) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `rekening`
 --
 
-INSERT INTO `rekening` (`nomor_rekening`, `id_pengguna`, `aktif`) VALUES
-('0002-01-011987-11-1', 'AB12C-ABCHD-8ABCD-88CAB', '1'),
-('0002-01-011987-11-2', 'AB12C-ABCHD-8ABCD-88CAC', '1');
+INSERT INTO `rekening` (`nomor_rekening`, `nama_pengguna`, `aktif`) VALUES
+('0002-01-011987-11-1', 'user', '1'),
+('0002-01-011987-11-2', 'user2', '1');
 
 -- --------------------------------------------------------
 
@@ -143,12 +122,12 @@ CREATE TABLE `transaksi` (
   `id` varchar(25) NOT NULL PRIMARY KEY,
   `waktu` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `jenis_transaksi` char(2) NOT NULL,
-  `rekening_asal` varchar(25) DEFAULT NULL,
+  `rekening_asal` varchar(25) NOT NULL,
   `rekening_tujuan` varchar(25) DEFAULT NULL,
   `nominal` decimal(12,2) NOT NULL,
   FOREIGN KEY(jenis_transaksi) REFERENCES jenis_transaksi(id) ON UPDATE CASCADE,
-  FOREIGN KEY(rekening_asal) REFERENCES rekening(nomor_rekening) ON DELETE SET NULL ON UPDATE CASCADE,
-  FOREIGN KEY(rekening_tujuan) REFERENCES rekening(nomor_rekening) ON DELETE SET NULL ON UPDATE CASCADE
+  FOREIGN KEY(rekening_asal) REFERENCES rekening(nomor_rekening) ON UPDATE CASCADE,
+  FOREIGN KEY(rekening_tujuan) REFERENCES rekening(nomor_rekening) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
