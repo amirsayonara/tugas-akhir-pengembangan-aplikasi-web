49312f2da709bbe1d1077364f87ed2f739156e5a
Amir Sayonara <amirsayonara@gmail.com>
2018-11-07 05:08:42 +0700

    perbaikan algoritma login dan info rekening

--- includes/sudah-masuk.php
+++ includes/sudah-masuk.php
@@ -1,3 +1,10 @@
 <?php
 
-if (!isset($_SESSION['nama-pengguna'])) header('Location: login');
+if (isset($_SESSION['nama-pengguna'])) {
+    $q = $conn->prepare("SELECT * FROM pengguna WHERE nama_pengguna='{$_SESSION['nama-pengguna']}' AND aktif='1'");
+    $q->execute();
+    if (!@$q->fetchAll()) {
+        unset($_SESSION['nama-pengguna']);
+        header('Location: login');
+    }
+} else header('Location: login');
