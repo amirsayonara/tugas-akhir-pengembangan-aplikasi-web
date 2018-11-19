84524a8208bbac5f628e15b6d53373f8c434d8d6
Amir Sayonara <amirsayonara@gmail.com>
2018-11-19 09:03:46 +0700

    perubahan layout login dan perubahan css

--- includes/login-content.php
+++ includes/login-content.php
@@ -5,26 +5,16 @@
                     Pengambilan captcha diambil dari kode php yang headernya diganti img (gambar)
                 -->
                 <h2>Login</h2>
-                <form method="POST">
-                    <table>
-                        <tr>
-                            <td><span class="pesan-error"><?=@$pesan_error['sistem']?></span></td>
-                        </tr><tr>
-                            <td><input type="text" name="username" id="username" value="<?=@$_POST['username']?>" placeholder="Username"></td>
-                        </tr><tr>
-                            <td><span class="pesan-error"><?=@$pesan_error['username']?></span></td>
-                        </tr><tr>
-                            <td><input type="password" name="password" id="password" value="<?=@$_POST['password']?>" placeholder="Password"></td>
-                        </tr><tr>
-                            <td><span class="pesan-error"><?=@$pesan_error['password']?></span></td>
-                        </tr><tr>
-                            <td><img src="captcha" alt="captcha"></td>
-                        </tr><tr>
-                            <td><input type="text" name="captcha" id="captcha" value="<?=@$_POST['captcha']?>" placeholder="Captcha"></td>
-                        </tr><tr>
-                            <td><span class="pesan-error"><?=@$pesan_error['captcha']?></span></td>
-                        </tr><tr>
-                            <td><input type="submit" value="Login"> <input type="reset" value="Reset"></td>
-                        </tr>
-                    </table>
-                </form>
+                <div class="form-login">
+                    <form method="POST">
+                        <span class="pesan-error"><?=@$pesan_error['sistem']?></span>
+                        <input type="text" name="username" id="username" value="<?=@$_POST['username']?>" placeholder="Username">
+                        <span class="pesan-error"><?=@$pesan_error['username']?></span>
+                        <input type="password" name="password" id="password" value="<?=@$_POST['password']?>" placeholder="Password">  
+                        <span class="pesan-error"><?=@$pesan_error['password']?></span><br>
+                        <img class="captcha" src="captcha" alt="captcha">
+                        <input type="text" name="captcha" id="captcha" value="<?=@$_POST['captcha']?>" placeholder="Captcha">
+                        <span class="pesan-error"><?=@$pesan_error['captcha']?></span><br>
+                        <input type="submit" value="Login"> <input type="reset" value="Reset">
+                    </form>
+                </div>
